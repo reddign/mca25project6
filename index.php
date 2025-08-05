db32cba7a4b9f7b4a1158c01d8cdf9a385bc9ba9
Abdullah Sultan <sultan@etown.edu>
2025-08-05 09:44:08 -0400

    change index page

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button href="signup.html"> Sign Up</button>
+    <button href="login.html"> Log In</button>
+    <button href="logout.html"> Log Out</button>
+    <button href="homepage.html"> Play Games</button>
+</body>
+</html>
