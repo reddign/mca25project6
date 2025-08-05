eeb113c4629e9e21273f36590d8a7801f629d9b1
Abdullah Sultan <sultan@etown.edu>
2025-08-05 13:52:47 -0400

    added dorkly image to js and made oblog shape

--- doodle/doodleJump.js
+++ doodle/doodleJump.js
@@ -1,15 +1,53 @@
-var canvas = document.querySelector("gameCanvas");
+var canvas = document.querySelector("canvas");
 var graphics = canvas.getContext("2d");
 
-const image = new Image();
-image.src = "doodlejumpdorkly.png";
+const doodleImg = new Image();
+doodleImg.src = "Doodlejumpdorkly.webp";
+
+var dorklyY;
+var dorklyX;
 
 
 function animate(){
+    
+    draw();
+    platform();
+}
+
+ doodleImg.onload = function() {
+    setInterval(animate, 1000 / 60); // 60 FPS
+};
+
+function draw() {
+    graphics.clearRect(0, 0, canvas.width, canvas.height);
+    
+    if (dorklyY === undefined) {
+        dorklyY = 100;
+    }
+    if (dorklyX === undefined) {
+        dorklyX = 100;
+    }
+    
+    graphics.drawImage(doodleImg, dorklyX, dorklyY, 50, 50);
+    
+    dorklyY += 2; // Gravity effect
+    if (dorklyY > canvas.height - 50) {
+        dorklyY = canvas.height - 50; // Prevent going off the bottom
+    }
 
 }
 
-
-function draw() {
+function platform(){
+    var oblong = {
+        x: 100,
+        y: 200,
+        width: 150,
+        height: 20
+    };
+    graphics.fillStyle = 'green';
+    graphics.beginPath();
+    graphics.ellipse(oblong.x + oblong.width / 2, oblong.y + oblong.height / 2, oblong.width / 3, oblong.height / 4, 0, 0, 2 * Math.PI);
     
+    graphics.fill();
+    graphics.closePath();
 }
