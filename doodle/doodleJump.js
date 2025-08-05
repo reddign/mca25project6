48b68be4bdeb9360887e1dd5938fe11bc9ea986c
Abdullah Sultan <sultan@etown.edu>
2025-08-05 13:26:20 -0400

    ..

--- doodle/doodleJump.js
+++ doodle/doodleJump.js
@@ -1,7 +1,14 @@
+var canvas = document.querySelector("gameCanvas");
+var graphics = canvas.getContext("2d");
+
+const image = new Image();
+image.src = "doodlejumpdorkly.png";
+
 
 function animate(){
 
 }
+
 
 function draw() {
     
