eab8d86ddf9c162bd58363c9867f5aadadfbafef
Nathan Andemichael <andemichael@etown.edu>
2025-08-05 10:42:09 -0400

    grass not road

--- flappy/flappy.js
+++ flappy/flappy.js
@@ -2,9 +2,9 @@ let canvas = document.querySelector("canvas");
 console.log("canvas");
 const graphics = canvas.getContext("2d");
 
-function road1(){
-    graphics.fillStyle="gray";
-    graphics.fillRect(100,100, canvas.width,100);
+function grass(){
+    graphics.fillStyle="green";
+    graphics.fillRect(0,140, canvas.width,20);
 }
 
-road1();
+grass();
